a7bd491986d5808368267fa9d0eccccd2be3231e
zhangzhongliang <zhangzhongliangzzl@gmail.com>
2024-10-08 00:37:13 +0800

    leetcode 236

---
+++ leetcode/236.go
@@ -0,0 +1,35 @@
+package leetcode
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	var ans *TreeNode
+
+	merge := func(m1, m2 map[int]bool) map[int]bool {
+		if m1 == nil {
+			if m2 == nil {
+				return map[int]bool{}
+			}
+			return m2
+		}
+		for k := range m2 {
+			m1[k] = true
+		}
+		return m1
+	}
+	var dfs func(node *TreeNode) map[int]bool
+	dfs = func(node *TreeNode) map[int]bool {
+		if node == nil || ans != nil {
+			return nil
+		}
+
+		sawLeft := dfs(node.Left)
+		sawRight := dfs(node.Right)
+		saw := merge(sawLeft, sawRight)
+		saw[node.Val] = true
+		if saw[p.Val] && saw[q.Val] && ans == nil {
+			ans = node
+		}
+		return saw
+	}
+	dfs(root)
+	return ans
+}
