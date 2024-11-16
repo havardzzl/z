14a050601df23525da3429e8cbfb39b257f0e30e
zhangzhongliang <zhangzhongliangzzl@gmail.com>
2024-11-16 23:56:43 +0800

    leetcode 876

---
+++ leetcode/876.go
@@ -0,0 +1,10 @@
+package leetcode
+
+func middleNode(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
