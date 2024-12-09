6b5f161b4dcee3a3b014ff56040268d81ef15b6b
zhangzhongliang <zhangzhongliangzzl@gmail.com>
2024-12-10 01:07:40 +0800

    leetcode 1143

---
+++ leetcode/1143.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	l1, l2 := len(text1), len(text2)
+	dp := make([][]int, l1+1)
+	for i := 0; i <= l1; i++ {
+		dp[i] = make([]int, l2+1)
+	}
+	ans := 0
+	for i := 0; i <= l1; i++ {
+		for j := 0; j <= l2; j++ {
+			if i == 0 || j == 0 {
+				continue
+			}
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+			ans = max(ans, dp[i][j])
+		}
+	}
+	return ans
+}
