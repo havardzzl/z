04528ba0c2b1f637c2ae1e56b058dcc6acfc99cc
zhangzhongliang <zhangzhongliangzzl@gmail.com>
2025-01-19 11:42:01 +0800

    leetcode 560(optimize)

--- leetcode/560.go
+++ leetcode/560.go
@@ -2,6 +2,24 @@ package leetcode
 
 func subarraySum(nums []int, k int) int {
 	n := len(nums)
+	sums := make([]int, n+1)
+	for i := range nums {
+		sums[i+1] = nums[i] + sums[i]
+	}
+	ans := 0
+	cnt := map[int]int{0: 1}
+	for i := 1; i < n+1; i++ {
+		ans += cnt[sums[i]-k]
+		cnt[sums[i]] += 1
+	}
+	return ans
+}
+
+/*
+O(n**2)版本
+func subarraySum(nums []int, k int) int {
+	n := len(nums)
+	// sums 保存以每个数字为起始位置的字数组和
 	sums := make([]int, n)
 	ans := 0
 	for i := range nums {
@@ -14,3 +32,4 @@ func subarraySum(nums []int, k int) int {
 	}
 	return ans
 }
+*/
