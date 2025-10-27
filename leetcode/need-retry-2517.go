d0ef0c63513678b267dae731828e58e8417da358
zhangzhongliang <zhangzhongliangzzl@gmail.com>
2025-10-27 12:17:36 +0800

    leetcode 2517

---
+++ leetcode/need-retry-2517.go
@@ -0,0 +1,21 @@
+package leetcode
+
+import (
+	"slices"
+	"sort"
+)
+
+func maximumTastiness(price []int, k int) int {
+	slices.Sort(price)
+	return sort.Search((price[len(price)-1]-price[0])/(k-1), func(d int) bool {
+		d++ // 二分最小的 f(d+1) < k，从而知道最大的 f(d) >= k
+		cnt, pre := 1, price[0]
+		for _, p := range price[1:] {
+			if p-pre >= d {
+				cnt++
+				pre = p
+			}
+		}
+		return cnt < k
+	})
+}
