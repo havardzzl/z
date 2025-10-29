118bdd2042ac1894159f014999a5a9ab47d9ef5b
zhangzhongliang <zhangzhongliangzzl@gmail.com>
2025-10-29 12:49:52 +0800

    leetcode 74

---
+++ leetcode/74.go
@@ -0,0 +1,11 @@
+package leetcode
+
+import "sort"
+
+func searchMatrix2(matrix [][]int, target int) bool {
+	m, n := len(matrix), len(matrix[0])
+	i := sort.Search(m*n, func(i int) bool {
+		return matrix[i/n][i%n] >= target
+	})
+	return i < m*n && matrix[i/n][i%n] == target
+}
